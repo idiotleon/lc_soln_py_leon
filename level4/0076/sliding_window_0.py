673174c51454a8f5d0bb693a0f754f47a98139bd
Leon <yanglyu.leon.7@gmail.com>
2024-02-04 11:48:23 -0800

    review

---
+++ level4/0076/sliding_window_0.py
@@ -0,0 +1,43 @@
+"""
+    https://leetcode.com/problems/minimum-window-substring
+
+    Reference:
+        https://leetcode.com/problems/minimum-window-substring/solutions/4673541/beats-100-explained-any-language-by-prodonik/
+
+    @author: Leon
+"""
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not s or not t or len(s) < len(t):
+            return ""
+
+        freqs = [0] * 128
+        for ch in t:
+            freqs[ord(ch)] += 1
+
+        LEN = len(s)
+        count = len(t)
+        lo = 0
+        hi = 0
+        lo_ans = 0
+        shortest = LEN
+
+        while hi < len(s):
+            if freqs[ord(s[hi])] > 0:
+                count -= 1
+            freqs[ord(s[hi])] -= 1
+
+            while count == 0:
+                if not shortest or hi - lo < shortest:
+                    shortest = hi - lo
+                    lo_ans = lo
+
+                if freqs[ord(s[lo])] == 0:
+                    count += 1
+                freqs[ord(s[lo])] += 1
+                lo += 1
+
+            hi += 1
+        return "" if shortest == LEN else s[lo_ans: lo_ans + shortest + 1]
